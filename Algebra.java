a0265666a1ff8a9b3c766283b3ffd8d5eca8c449
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-11-27 15:28:19 +0200

    Completed Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -8,7 +8,7 @@ public class Algebra {
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(12,-3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
@@ -17,6 +17,19 @@ public class Algebra {
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
 	}  
+	public static int abs(int x) {
+		int x2 = x;
+		int count = 0;
+		if (x2 >= 0) {
+			return x2;
+		} else if (x2 < 0) {
+			while (x2 < 0) {
+				x2++;
+				count++;
+			}
+		}
+		return count;
+	}
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
@@ -56,88 +69,93 @@ public class Algebra {
 		return x1;
 	}
 
+	
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-	int newX = x1;
-	int count = 0;
-	if (x1 == 0 || x2 == 0) {
-		x1 = 0;
-	} else if (x2 < 0 && x1 > 0) {
-		while (x2 < 0) {
-			x2++;
-			count++;
+		int newX1 = x1;
+        int result = 0;
+        int count = 0;
+        if (newX1 < 0) {
+			while (newX1 < 0) {
+				newX1++;
+				count++;
+			}
+            newX1 = count;
 		}
-		for (int i = 1 ; i < count; i++) {
-			x1 = plus(x1,newX);
+        int newX2 = x2;
+		count = 0;
+        if (newX2 < 0) {
+			while (newX2 < 0) {
+				newX2++;
+				count++;
+			}
+            newX2 = count;
 		}
-		x1 = minus(0, x1);
-	} else if (x1 < 0 && x2 > 0) {
-		while (x1 < 0) {
-			x1++;
-			count++;
-		}
-		x1 = count;
-		for (int i = 1 ; i < x2; i++) {
-			x1 = plus(x1,newX);
-		}
-		x1 = minus(0, x1);
-	} else { 
-		for (int i = 1 ; i < x2; i++) {
-			x1 = plus(x1,newX);
-		}
-	} 
-		return x1;
-	} 
+        if (x1 == 0 || x2 == 0) {
+            return 0;
+        } 
+        for (int i = 1 ; i <= newX2; i++) {
+            result = plus(result, newX1);
+        }
+        if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)) {
+            result = minus(0, result);
+            }
+        return result; 
+        }
+    
 	
 	
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
+		int newX = x;
 		if (n > 0) {
-			int newX = x;
-				for (int i = 1 ; i < n; i++) {
-					x = times(x,newX);
-			} 
+			for (int i = 1 ; i < n; i++) {
+				newX = times(x,newX);
+			}	
 		} else if (n == 0) {
-			x = 1;
-		} else if (x < 0 && mod(x,-2) == 1) {
-			int newX2 = x;
-				for (int i = 1 ; i < n; i++) {
-					x = times(x,newX2);
-				}
-			x = minus(0, x);
+			newX = 1;
 		}	
-			return x;
+		return newX;
 
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int count = 1;
-		int newX = x2;
-		if (x1 < 0 && x2 > 0) {
-			while (x1 < 0) {
-				x1++;
+		int count = 0;
+		int result = 0;
+		int newX2 = x2;
+		if (newX2 < 0) {
+			while (newX2 < 0) {
+				newX2++;
 				count++;
 			}
-		} else if (x2 < 0 && x1 > 0) {
-			while (x2 < 0) {
-				x2++;
-				count++;
-			}
+			newX2 = count;
 		}
-			while (x1 >= x2) {
-				x2 = plus(x2,newX);
+		int newX1 = x1;
+		count = 0;
+		if (newX1 < 0) {
+			while (newX1 < 0) {
+				newX1++;
 				count++;
 			}
-				if (x2 > x1) {
-					count = minus(count,1);
-				}
-			if ((x1 < 0 && x2 > 0) || (x2 < 0 && x1 > 0)) {
-				count = minus(0, count);
-			}
-		return count;
-	}
+			newX1 = count;
+		}
+		count = 0;
+		result = 0;
+		while (newX1 >= result) {
+			result += newX2;
+			count++;
+		}
+		if (result > newX1) {
+			count = count - 1;
+		}
+		if ((x1 < 0 && x2 > 0) || (x2 < 0 && x1 > 0)) {
+			count = 0 - count;
+		}
+	return count;
+}
+
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
